b62a47ed18a1054569c48141dd38bd2226f285f9
Bassem Sadaqah <bassem.sadaqah@gmail.com>
2022-12-22 22:20:22 +0200

    Add simple frontend to test the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
+import "./App.css";
+
+function get_products(setProducts) {
+  fetch(
+    "http://udacity-storefront-backend-prod.eba-vmtydbqk.us-east-1.elasticbeanstalk.com/products"
+  )
+    .then(res=>{
+      setProducts(res.json())
+      console.log(res.json())
+    }).catch(res=>{
+      setProducts("Couldn't Get Products")
+    })
+}
 function App() {
+    const [products, setProducts] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>get_products(setProducts)}>Get Products</button>
+      <p>{products.toString()}</p>
     </div>
   );
 }
